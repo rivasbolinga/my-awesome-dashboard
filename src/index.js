34faf701cfc593a32b33fb8657cf0dafebe4d7ca
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-07-30 11:59:23 +0200

    First commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { Auth0Provider } from '@auth0/auth0-react'
+import App from './App'
+
+const root = createRoot(document.getElementById('root'))
+
+root.render(
+  <Auth0Provider
+    domain="dev-635ounsp7aik3c5z.us.auth0.com"
+    clientId="pKHzgGDQX1MuRJVrSGOdSRXVNwQX9p9O"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <App />
+  </Auth0Provider>
+)
